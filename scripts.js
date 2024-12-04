d4757e9f2b57b0d01341e4b4ff102c274145fcce
Keago99 <keatabde@gmail.com>
2024-12-04 18:18:24 +0200

    added base functionality to operate function. will replace console.logs with actual functionality

--- scripts.js
+++ scripts.js
@@ -1,15 +1,48 @@
 const screen = document.querySelector("#calcScreen");
 const num = document.querySelectorAll(".num");
+const opperators = document.querySelectorAll(".operator");
 const clear = document.querySelector("#clear");
+var operators = {};
+var numbers = {};
+
+
+function operate(){
+    if (screen.innerText != "" && screen.innerText.length < 9){
+        switch (this.innerText){
+            case "+":
+                console.log("its a plus");
+                break;
+            
+            case "-":
+                console.log("its a minus");
+                break;
+            
+            case "%":
+                console.log("its a divide");
+                break;
+
+            case "X":
+                console.log("its a multiplication");
+                break;
+            default:
+                console.log("who knows what it is");
+        }
+    }
+}
 
 function clearScreen(){
     screen.innerText = "";
 }
 
-
 function addToScreen(){
     screen.append(this.innerText);
 }
+
+for (let i = 0; i < opperators.length; i++){
+    opperators[i].addEventListener("click", operate);
+}
+
+
 
 for (let i = 0; i < num.length; i++){
     num[i].addEventListener("click", addToScreen);
