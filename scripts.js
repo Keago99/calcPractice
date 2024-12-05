223fe7076a1a731fa4922692c5e5dc2a616b66e1
Keago99 <keatabde@gmail.com>
2024-12-05 23:19:21 +0200

    add function for all operations, add rounding function with variable.toFixed(2) bunch of testing, to add press down highlight

--- scripts.js
+++ scripts.js
@@ -5,59 +5,92 @@ const clear = document.querySelector("#clear");
 const equalButton = document.querySelector("#equals");
 var opperatorArray = [];
 var numberArray = [];
+var busyCalc = false;
+
+function equals(){
+    if (numberArray.length === 1){
+        let equalsNumber = 0
+        equalsNumber = parseInt(screen.innerText);
+        numberArray.push(equalsNumber);
+        console.log(opperatorArray);
+        Calculate();
+        console.log(opperatorArray);
+        busyCalc = true;
+    }
+}
 
 function Calculate(){
-    if (numberArray.length < 1){
         let newNumber = 0;
         switch(opperatorArray[0]){
             case "+":
                 newNumber = numberArray[0] + numberArray[1];
+                numberArray.unshift(newNumber);
+                opperatorArray.shift();
+                numberArray.splice(-2);
                 break;
             
             case "-":
                 newNumber = numberArray[0] - numberArray[1];
+                numberArray.unshift(newNumber);
+                opperatorArray.shift();
+                numberArray.splice(-2);
                 break;
             
             case "X":
                 newNumber = numberArray[0] * numberArray[1];
+                numberArray.unshift(newNumber);
+                opperatorArray.shift();
+                numberArray.splice(-2);
                 break;
             
             case "%":
                 newNumber = numberArray[0] / numberArray[1];
+                numberArray.unshift(newNumber);
+                opperatorArray.shift();
+                numberArray.splice(-2);
                 break;
             
             default:
                 console.log("its a thing");
                 break;
         }
+        screen.innerText = numberArray[0].toFixed(2);
     }
-}
-
-function equals(){
-    if(screen.innerText !="" && (numberArray.length % 2 != 0)){
-        
-    }
-}
 
 function operate(){
     let numbertoNumbers = 0;
+    if (busyCalc ===  true){
+        opperatorArray.push(this.innerText);
+        screen.innerText = "";
+        busyCalc = false;
+    }
+
     if (screen.innerText != "" && screen.innerText.length < 9 && numberArray.length < 1){
         opperatorArray.push(this.innerText);
         numbertoNumbers = parseInt(screen.innerText);
         numberArray.push(numbertoNumbers);
-        clearScreen();
+        screen.innerText = "";
     }
-    else{
-        console.log("do something");
+    else if (screen.innerText.length < 9 && screen.innerText != "" && numberArray.length === 1){
+        opperatorArray.push(this.innerText);
+        numbertoNumbers = parseInt(screen.innerText);
+        numberArray.push(numbertoNumbers);
+        Calculate();
+        busyCalc = true;
     }
 }
 
 
 function clearScreen(){
     screen.innerText = "";
+    opperatorArray = [];
+    numberArray = [];
 }
 
 function addToScreen(){
+    if (busyCalc === true){
+        screen.innerText = "";
+    }
     screen.append(this.innerText);
 }
 
